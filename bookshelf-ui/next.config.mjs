a2c32509074d8b16ec69311d141b71ea90a0f0fb
C4rlos316 <cama619@gmail.com>
2025-05-12 04:06:06 +0000

    Se modifico el archivo package.json para agregar scripts de formateo y se agregó un Dockerfile para la configuración del contenedor. Se agregó un archivo .prettierrc para la configuración de Prettier.

--- bookshelf-ui/next.config.mjs
+++ bookshelf-ui/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    //Genera un directorio .next en la raiz del proyecto para saber donde se guardo la compilacion
+    output: 'standalone',
+};
 
 export default nextConfig;
